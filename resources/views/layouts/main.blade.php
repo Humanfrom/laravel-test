a5a3770a88d03e217bdb71375fd767f0dfc72e01
humanfrom <leonovton@gmail.com>
2022-09-27 14:45:39 +0300

    init

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Test React-JWT-Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+  </head>
+  <body>
+    <div id="root">
+        @yield("content")
+      <div>
+  </body>
+</html>
